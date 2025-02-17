8ce1f3827c6c0cfe61b5749fa8383ad9f8aba7b9
RavikiranGoru <ravikirangoru@gmail.com>
2025-02-17 23:02:32 +0530

    dialog box

---
+++ 7_dialog_box.js
@@ -0,0 +1,14 @@
+// Function to change the content of the paragraph
+function callAllert() {
+    alert("You just clicked it!")
+}
+function callPrompt() {
+    let age = prompt("Enter Your age!")
+    document.write("<br/> Entered age:", age)
+}
+function callConfirm() {
+    let flag = confirm("Are you sure?");
+    document.write("<br/> Your confirmation:", flag)
+}
+
+// You can add more functions and logic here to practice different JS features.
